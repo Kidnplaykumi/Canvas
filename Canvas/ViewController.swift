c8f17f8a787978823da51d2405ebe690618865ca
Kidnplaykumi <elijah8512@yahoo.com>
2018-03-13 16:05:38 -0400

    Animation Problem

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var trayView: UIView!
     var trayOriginalCenter: CGPoint!
+    var velocity = sender.velocity(in: view)
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        trayDownOffset = 160
+        trayUp = trayView.center // The initial position of the tray
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +32,7 @@ class ViewController: UIViewController {
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
         let translation = sender.translation(in: view)
         print("translation \(translation)")
+        
         if sender.state == .began {
             trayOriginalCenter = trayView.center
             
@@ -32,8 +40,13 @@ class ViewController: UIViewController {
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
             
         } else if sender.state == .ended {
-            
+           
         }
+        
+        UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                       animations: { () -> Void in
+                        self.trayView.center = self.trayDown
+        }, completion: nil)
     }
     
 
