f9630059c5f83cc58a4c74c210d671dfab41dfab
Kidnplaykumi <elijah8512@yahoo.com>
2018-03-18 14:11:45 -0400

    Prblems

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -11,15 +11,18 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var trayView: UIView!
     var trayOriginalCenter: CGPoint!
-    var velocity = sender.velocity(in: view)
     var trayDownOffset: CGFloat!
     var trayUp: CGPoint!
     var trayDown: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        trayDownOffset = 160
+        trayDownOffset = 215
         trayUp = trayView.center // The initial position of the tray
         trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
     }
@@ -30,23 +33,39 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        
+        var velocity = sender.velocity(in: view)
+        
         let translation = sender.translation(in: view)
         print("translation \(translation)")
         
         if sender.state == .began {
             trayOriginalCenter = trayView.center
+            let imageView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            
             
         } else if sender.state == .changed {
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
             
         } else if sender.state == .ended {
-           
+            if velocity.y > 0.0 {
+                self.trayView.center = self.trayDown
+            } else {
+                self.trayView.center = self.trayUp
+            }
         }
         
-        UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
-                       animations: { () -> Void in
-                        self.trayView.center = self.trayDown
-        }, completion: nil)
+        
+    }
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        
+        
     }
     
 
